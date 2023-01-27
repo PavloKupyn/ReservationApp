15fddfafe77737cd939f0a3674db047e7bc0b4d6
Pavlo Kupyn <pavlokypyn7@gmail.com>
2023-01-27 15:12:42 +0200

    Initial Commit

---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Eric Cartman on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
